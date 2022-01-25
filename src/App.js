6e800701c7980353a4c193036cc53170ad76d9f7
Thiago <coutocodes@gmail.com>
2022-01-25 12:30:20 -0500

    Created user context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useContext } from 'react'
 import logo from './logo.svg';
 import './App.css';
 import Nav from './componets/Nav'
+import UserContext from './contexts/UserContext'
 
 function App() {
+  // const user = useContext(UserContext)
+  // console.log(user)
   return (
     <div className="App">
-      <Nav />
+      {/* All context comes with the Provider Componet.  This allows us to use this as a wrapper and share information to all of its children.  We need the value prop inside our provider. */}
+      <UserContext.Provider value={'Thiago'}>
+        <Nav />
+      </UserContext.Provider>
+
+
     </div>
   );
 }
---
+++ src/contexts/UserContext.js
@@ -0,0 +1,8 @@
+// import createContext
+import React, { createContext } from 'react'
+
+// Simplest way to create a context
+// Standard practice: name your context the same as your file
+const UserContext = createContext('Thiago')
+
+export default UserContext
