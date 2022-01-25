dc23a99d6c1070b08274bde8d319c0bf713c045b
Thiago <coutocodes@gmail.com>
2022-01-25 11:41:30 -0500

    Rendering nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './componets/Nav'
 
 function App() {
   return (
     <div className="App">
-      Hello, World!
+      <Nav />
     </div>
   );
 }
